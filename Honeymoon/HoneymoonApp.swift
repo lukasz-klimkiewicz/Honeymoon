d2361ca73c5dd2fac7a71943ba57dad2db506087
Łukasz Klimkiewicz <lklimkiewicz@icloud.com>
2021-12-29 12:08:26 +0100

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Łukasz Klimkiewicz on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
