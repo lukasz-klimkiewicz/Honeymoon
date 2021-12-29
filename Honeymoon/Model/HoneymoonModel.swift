ce32ad62e9af0e36f0e357638166709af0fce837
Łukasz Klimkiewicz <lklimkiewicz@icloud.com>
2021-12-29 13:26:30 +0100

    1. Project setup 2. Model 3. Data 4. Card view

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,19 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Łukasz Klimkiewicz on 29/12/2021.
+//
+
+import Foundation
+
+struct Destination {
+    
+    var place: String
+    var country: String
+    var image: String
+    
+}
+
+
+
